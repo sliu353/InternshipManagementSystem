f362fafa1aa908da7467d1dc5181d555eaf5dd8f
Siyuan Liu <sliu353@UoA.auckland.ac.nz>
2016-04-23 21:01:44 +1200

    Substitude localDB with azure database for authentication.

--- InternshipManagementSystem/Models/IdentityModels.cs
+++ InternshipManagementSystem/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace InternshipManagementSystem.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("azure", throwIfV1Schema: false)
         {
         }
 
